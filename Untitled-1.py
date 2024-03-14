ae2da54a40ca2ca3c7952c0d1e4a179d8c0c02f5
bigboythe4th <nw081408@wgcloud.org>
2024-03-14 11:11:15 -0500

    new class

---
+++ Untitled-1.py
@@ -0,0 +1,107 @@
+# Your code here...
+class Vehicle:
+    def __init__(self):
+        self.price = 0
+        self.category = ''
+    
+    def __repr__(self):
+        return "category:" + self.category + ",price:" + str(self.price)
+    
+        
+class Car(Vehicle):
+    
+    def __init__(self):
+        super().__init__()
+        self.category = 'car'
+
+class boat(Vehicle):
+    
+    def __init__(self):
+        super().__init__()
+        self.category = 'boat'
+        
+        
+class Truck(Car):
+    
+    def __init__(self):
+        super().__init__() 
+        self.price = 40000
+        
+class pickup_truck(Truck):
+    
+    def __init__(self):
+        super().__init__() 
+        self.price = 35000
+
+
+class Nissan_Frontier(pickup_truck):
+    
+    def __init__(self):
+        super().__init__() 
+        self.price = 28000
+
+
+class semi_truck(Truck):
+    
+    def __init__(self):
+        super().__init__() 
+        self.price = 150000
+
+class kenworth_t880(semi_truck):
+    
+    def __init__(self):
+        super().__init__() 
+        self.price = 180000
+
+
+
+
+class sports_car(Car):
+    
+    def __init__(self):
+        super().__init__() 
+        self.price = 70000
+
+class exotic_car(sports_car):
+    
+    def __init__(self):
+        super().__init__() 
+        self.price = 500000
+
+class mclaren_p1 (exotic_car):
+    
+    def __init__(self):
+        super().__init__() 
+        self.price = 1500000
+
+class suv(Car):
+    
+    def __init__(self):
+        super().__init__() 
+        self.price = 32000
+        
+
+class sedan(Car):
+    
+    def __init__(self):
+        super().__init__() 
+        self.price = 23000
+
+
+class sedan(Car):
+    
+    def __init__(self):
+        super().__init__() 
+        self.price = 23000
+
+class coupe(Car):
+    
+    def __init__(self):
+        super().__init__() 
+        self.price = 24000
+
+class van(Car):
+    
+    def __init__(self):
+        super().__init__() 
+        self.price = 32000
